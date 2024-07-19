c0e6d4ae385ea1c686ecbb6d91db2b31b0c5e09b
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-07-19 17:13:27 +0530

    Update StdinandstdoutII.java

--- HackerRank/StdinandstdoutII.java
+++ HackerRank/StdinandstdoutII.java
@@ -1,5 +1,16 @@
 package HackerRank;
-
+import java.util.Scanner;
 public class StdinandstdoutII {
 
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        scan.nextLine(); //thats check the text with space 
+        String s= scan.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
 }
