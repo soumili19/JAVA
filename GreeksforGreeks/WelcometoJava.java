3db3ad1db88dd1af7ac6a4a058086d691ddedc0d
SOUMILI DEY <132081345+soumili19@users.noreply.github.com>
2024-07-19 15:07:31 +0530

    Add files via upload

---
+++ GreeksforGreeks/WelcometoJava.java
@@ -0,0 +1,9 @@
+package GfgCode;
+
+public class WelcometoJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
